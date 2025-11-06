a3086a5150524748a06fdbc31bc98bc1bd68e9d3
Julie <just0003@stud.ek.dk>
2025-11-06 12:38:56 +0100

    Instruction log

---
+++ js/instruction.js
@@ -0,0 +1,7 @@
+console.log("Added instructions.js");
+
+document.querySelector("#energidrik").addEventListener("click", energidrikF);
+
+function energidrikF() {
+  console.log("energidrikF er added");
+}
